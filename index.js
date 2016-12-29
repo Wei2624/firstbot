882c431f405c8f40768ab3eb43a1db9ec86c2006
wei zhang <Wei@Weis-MacBook-Pro.local>
2016-12-28 20:56:29 -0500

    new index

--- index.js
+++ index.js
@@ -101,42 +101,6 @@ const findOrCreateSession = (fbid) => {
   return sessionId;
 };
 
-// Our bot actions
-const actions = {
-  send({sessionId}, {text}) {
-    // Our bot has something to say!
-    // Let's retrieve the Facebook user whose session belongs to
-    const recipientId = sessions[sessionId].fbid;
-    if (recipientId) {
-      // Yay, we found our recipient!
-      // Let's forward our bot response to her.
-      // We return a promise to let our bot know when we're done sending
-      return fbMessage(recipientId, text)
-      .then(() => null)
-      .catch((err) => {
-        console.error(
-          'Oops! An error occurred while forwarding the response to',
-          recipientId,
-          ':',
-          err.stack || err
-        );
-      });
-    } else {
-      console.error('Oops! Couldn\'t find user for session:', sessionId);
-      // Giving the wheel back to our bot
-      return Promise.resolve()
-    }
-  },
-  // You should implement your custom actions here
-  // See https://wit.ai/docs/quickstart
-};
-
-// Setting up our bot
-const wit = new Wit({
-  accessToken: WIT_TOKEN,
-  actions,
-  logger: new log.Logger(log.INFO)
-});
 
 // Starting our webserver and putting it all together
 const app = express();
@@ -204,7 +168,7 @@ app.post('/webhook', (req, res) => {
             		return context;
           			},
       		};
-      		const client = new Wit({accessToken, actions});
+      		const client = new Wit({WIT_TOKEN, actions});
 			interactive(client);
           }
         } else {
