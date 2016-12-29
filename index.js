2e83fd3b3fba50b2c08020588ec85337b6640129
wei zhang <Wei@Weis-MacBook-Pro.local>
2016-12-28 22:16:47 -0500

    new index

--- index.js
+++ index.js
@@ -62,6 +62,19 @@ const fbMessage = (id, text) => {
   });
 };
 
+
+const firstEntityValue = (entities, entity) => {
+  const val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][0].value
+  ;
+  if (!val) {
+    return null;
+  }
+  return typeof val === 'object' ? val.value : val;
+};
+
 // ----------------------------------------------------------------------------
 // Wit.ai bot specific code
 
