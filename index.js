961b1c849563e95dfd14f12707ce6f8c67229fb2
300keatsway <Wei@Weis-MacBook-Pro.local>
2017-01-01 18:58:39 -0500

    new version

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const fbMessage = (id, text) => {
     recipient: { id },
     message: { text },
   });
-  const qs_back = 'access_token=' + encodeURIComponent(FB_PAGE_TOKEN);
+  const qs = 'access_token=' + encodeURIComponent(FB_PAGE_TOKEN);
   return fetch('https://graph.facebook.com/me/messages?' + qs, {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
