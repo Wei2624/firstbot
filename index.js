a7b0b61e390884e80bad7ce7439a2c5d87d8456d
300keatsway <Wei@Weis-MacBook-Pro.local>
2017-01-02 20:17:42 -0500

    new version

--- index.js
+++ index.js
@@ -45,8 +45,8 @@ crypto.randomBytes(8, (err, buff) => {
 
 const fbMessage = (id, text) => {
   const body = JSON.stringify({
-    recipient:  id ,
-    message:  text ,
+    recipient:  {id} ,
+    message:  {text },
   });
   const qs = 'access_token=' + encodeURIComponent(FB_PAGE_TOKEN);
   return fetch('https://graph.facebook.com/me/messages?' + qs, {
@@ -135,16 +135,16 @@ const actions = {
       // Yay, we found our recipient!
       // Let's forward our bot response to her.
       // We return a promise to let our bot know when we're done sending
-      return fbMessage(recipientId, text)
-      .then(() => null)
-      .catch((err) => {
-        console.error(
-          'Oops! An error occurred while forwarding the response to',
-          recipientId,
-          ':',
-          err.stack || err
-        );
-      });
+      // return fbMessage(recipientId, text)
+      // .then(() => null)
+      // .catch((err) => {
+      //   console.error(
+      //     'Oops! An error occurred while forwarding the response to',
+      //     recipientId,
+      //     ':',
+      //     err.stack || err
+      //   );
+      // });
     } else {
       console.error('Oops! Couldn\'t find user for session:', sessionId);
       // Giving the wheel back to our bot
