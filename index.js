759aea4963192f2a35ea51bb95c7fa70311c7419
300keatsway <Wei@Weis-MacBook-Pro.local>
2017-01-03 19:54:07 -0500

    new version

--- index.js
+++ index.js
@@ -132,6 +132,7 @@ const actions = {
     if (recipientId) {
       console.log(text);
       var obj = JSON.parse(text);
+      sendTextMessage(recipientId,obj);
       // console.log("the below is parsed obj");
       // console.log(obj);
       //let messageData = '{"attachment":{"type":"template","payload":{"template_type":"generic","elements":[{"title":"First card","subtitle":"Element #1 of an hscroll","image_url":"http://messengerdemo.parseapp.com/img/rift.png","buttons":[{"type":"web_url","url":"https://www.messenger.com","title":"web url"},{"type":"postback","title":"Postback","payload":"Payload for first element in a generic bubble"}]},{"title":"Second card","subtitle":"Element #2 of an hscroll","image_url":"http://messengerdemo.parseapp.com/img/gearvr.png","buttons":[{"type":"postback","title":"Postback","payload":"Payload for second element in a generic bubble"}]}]}}}'
